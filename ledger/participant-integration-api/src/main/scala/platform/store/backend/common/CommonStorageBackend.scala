08d201eadf1dec8a3b010c93023e3e885a652872
Kamil Bozek <kamil.bozek@digitalasset.com>
2021-09-19 21:12:53 +0200

    Removed use of participant_events view for max sequential id lookup

--- ledger/participant-integration-api/src/main/scala/platform/store/backend/common/CommonStorageBackend.scala
+++ ledger/participant-integration-api/src/main/scala/platform/store/backend/common/CommonStorageBackend.scala
@@ -708,4 +708,19 @@ private[backend] trait CommonStorageBackend[DB_BATCH] extends StorageBackend[DB_
 
   override def checkDatabaseAvailable(connection: Connection): Unit =
     assert(SQL"SELECT 1".as(get[Int](1).single)(connection) == 1)
+
+  override def maxEventSequentialIdOfAnObservableEvent(offset: Offset)(connection: Connection): Option[Long] = {
+    import com.daml.platform.store.Conversions.OffsetToStatement
+    SQL"""
+SELECT max_esi FROM (
+  (SELECT max(event_sequential_id) AS max_esi FROM participant_events_consuming_exercise WHERE event_offset <= $offset GROUP BY event_offset ORDER BY event_offset DESC FETCH NEXT 1 ROW ONLY)
+  UNION ALL
+  (SELECT max(event_sequential_id) AS max_esi FROM participant_events_non_consuming_exercise WHERE event_offset <= $offset GROUP BY event_offset ORDER BY event_offset DESC FETCH NEXT 1 ROW ONLY)
+  UNION ALL
+  (SELECT max(event_sequential_id) AS max_esi FROM participant_events_create WHERE event_offset <= $offset GROUP BY event_offset ORDER BY event_offset DESC FETCH NEXT 1 ROW ONLY)
+) t
+ORDER BY max_esi DESC
+FETCH NEXT 1 ROW ONLY
+       """.as(get[Long](1).singleOpt)(connection)
+  }
 }
